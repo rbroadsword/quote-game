f7ca43f8b7125dbb23e1e568a8280858034241e8
Nick Tse <nicholashtse@gmail.com>
2022-06-05 16:13:08 -0700

    Build UI and call API to populate questions and correct answer

--- src/index.js
+++ src/index.js
@@ -5,7 +5,38 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import QuoteService from './js/quote-service.js'; 
 
+function loopThroughQuestions() {
+  for (let i = 1; i < 10; i++) {
+    let submitAnswer = '#submitAnswer' + i;
+    let currentQuestion = '#question' + i;
+    let nextQuestion = '#question' + (i + 1);
+    let quoteText = '#quoteText' + (i + 1);
+    let correctAnswer = '#correctAnswer' + (i +1);
+
+    $(submitAnswer).click(function() {
+      $(currentQuestion).hide();
+      $(nextQuestion).show();
+      QuoteService.getQuote() 
+      .then(function(response) {
+        $(quoteText).text(response.content)
+        $(correctAnswer).text(response.author);
+      });
+    })
+  }
+}
 
 $(document).ready(function() {
+  $('#startGame').click(function() {
+    $('#submitAnswer').show();
+    $('#question1').show();
+    $('#startGame').hide();
 
+    QuoteService.getQuote() 
+      .then(function(response) {
+        $('#quoteText1').text(response.content)
+        $('#correctAnswer1').text(response.author);
+      });
+  });
+
+  loopThroughQuestions();
 }); 

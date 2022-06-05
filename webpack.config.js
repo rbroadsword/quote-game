4a78d8537f604c82145865e90e924a454cbcedd0
Ryan Broadsword <rbroadsword@gmail.com>
2022-06-05 11:30:54 -0700

    initial push

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'template',      // 
+      title: 'quote-game',      // 
       template: './src/index.html',
       inject: 'body'
     })

f080d79299d3f58c4fbd398dc4d62ed68a4a3974
Ryan Broadsword <rbroadsword@gmail.com>
2022-06-06 20:26:19 -0700

    add sound class, leaderboard

--- src/game.js
+++ src/game.js
@@ -1,13 +1,22 @@
 export default class Game {
-  constructor (question, answer){
-    this.question = question;
-    this.answer = answer;
-    this.rightAnswer = 0; 
+  constructor (player){
+    this.player = player; 
+    this.totalPoints = 0; 
+    this.leaderboard =  [ first = {name: "Nick", score: 15}, second = {name: "Victoria", score: 10}, third = {name: "Louie", score: 5}]; 
   }
 
   score() {
     console.log('score function'); 
-    return this.rightAnswer +=1; 
+    return this.totalPoints +=1; 
   }
+
+  topPlayers() {
+    if (this.totalPoints > this.leaderboard.first)
+  }
+
+
+
+
+
 
 }

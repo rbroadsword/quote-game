c2db7f10597818bbeffb9c5ec65677f87adfa8bb
Nick Tse <nicholashtse@gmail.com>
2022-06-12 09:25:33 -0700

    Update application to incorporate sound file for correct answer

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import $ from "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
+// import "./assets/sounds/Correct-sound-effect (1).mp3";
 import QuoteService from "./js/quote-service.js";
 import MovieQuoteService from "./js/moviequote-service";
 import Game from "./game.js";
-import Sounds from "./js/sounds.js";
 
 function answerIndex() {
   const randomAnswerIndex = Math.floor(Math.random() * 4) + 1;
@@ -28,10 +28,15 @@ function generateRandomQuote(newGame, randomAnswerIndex) {
   }
 }
 
-function checkAnswer(choiceNumber, newGame, mySound) {
+function playCorrectAnswerSound() {
+  let mySound = new Audio("./assets/img/Correct-sound-effect (1).mp3");
+  mySound.play();
+}
+
+function checkAnswer(choiceNumber, newGame) {
   if (document.getElementById("ans" + choiceNumber).checked) {
     newGame.score();
-    mySound.playCorrectAnswer();
+    playCorrectAnswerSound();
     console.log("this is total score " + newGame.rightAnswer);
     console.log("success");
   } else {
@@ -64,18 +69,12 @@ $(document).ready(function () {
     $("#randomQuote").hide();
     $("#movieQuote").hide();
     let newGame = new Game("", "");
-
     answerID = answerIndex();
     generateRandomQuote(newGame, answerID);
     $("#submitRandomAnswer").click(function () {
-      let mySound = new Sounds(
-        "../assets/img/sounds/Correct-sound-effect(1).mp3"
-      );
-      checkAnswer(answerID, newGame, mySound);
+      checkAnswer(answerID, newGame);
       $(".progress").html(`<div class="progress-bar" role="progressbar" 
-      style="width: ${
-        (newGame.turnCount / 5) * 100
-      }%;" aria-valuenow="${newGame.turnCount}" aria-valuemin="0" aria-valuemax="5"></div>
+      style="width: ${(newGame.turnCount / 5) * 100}%;" aria-valuenow="${newGame.turnCount}" aria-valuemin="0" aria-valuemax="5"></div>
     `);
       answerID = answerIndex();
       if (newGame.turnCount < 5) {
@@ -100,15 +99,9 @@ $(document).ready(function () {
     generateMovieQuote(answerID);
 
     $("#submitMovieAnswer").click(function () {
-      let mySound = new Sounds(
-        "../assets/img/sounds/Correct-sound-effect(1).mp3"
-      );
-      checkAnswer(answerID, newGame, mySound);
+      checkAnswer(answerID, newGame);
       $(".progress").html(`<div class="progress-bar" role="progressbar" 
-      style="width: ${
-        (newGame.turnCount / 5) * 100
-      }%;" aria-valuenow="${newGame.turnCount}" aria-valuemin="0" aria-valuemax="5"></div>
-    `);
+      style="width: ${(newGame.turnCount / 5) * 100}%;" aria-valuenow="${newGame.turnCount}" aria-valuemin="0" aria-valuemax="5"></div>`);
       answerID = answerIndex();
       if (newGame.turnCount < 5) {
         newGame.turnCount += 1;
